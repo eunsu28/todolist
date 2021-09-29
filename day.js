7e1512133878b6b1ee432ed185863c2530070509
eunsu28 <ayay.e2008@gmail.com>
2021-09-29 18:13:17 +0800

    error fixed

--- day.js
+++ day.js
@@ -1,20 +1,21 @@
 
 // querySelector
-const day = document.querySelector("h2#clock");
+const MDY = document.querySelector("h2#day");
 //
 
 // get time using date
 function getDay() {
     const date = new Date();
-    const month = String(date.getMonth()).padStart(2, "0");
-    const day = String(date.getDay()).padStart(2, "0");
+    const month = String((date.getMonth()) + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
     const year = String(date.getFullYear());
-    day.innerText = (`${month}/${day}/${year}`);
+    MDY.innerText = (`${month}/${day}/${year}`);
+    
 }
 //
 
 //start
 getDay();
-setInterval(getDay, 1000);
+setInterval(getDay, 100000);
 // setInterval(function, milli second)
 // set Timeout
