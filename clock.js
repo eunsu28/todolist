60a0d528153806d3723b60309295426ca040736d
eunsu28 <ayay.e2008@gmail.com>
2021-09-29 17:49:41 +0800

    add time

---
+++ clock.js
@@ -0,0 +1,19 @@
+// querySelector
+const clock = document.querySelector("h1#clock");
+//
+
+// get time using data.getHours / getMinutes / getSeconds
+function getTime() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = (`${hours}:${minutes}:${seconds}`);
+}
+//
+
+//start
+getTime();
+setInterval(getTime, 1000);
+// setInterval(function, milli second)
+// set Timeout
