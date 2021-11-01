1005c870c96022f4d8eaf0616d7a35a6c94ecf5d
eunsu28 <ayay.e2008@gmail.com>
2021-11-01 09:08:44 +0800

    show to html

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const form = document.querySelector("#input-place");
 const input = form.querySelector("input")
+const indexLi = document.getElementById("todo-list");
+
+function delate(){
+    console.log("delate")
+}
 
 function addTodo(todo){
-    console.log(todo)
+    const li = document.createElement("li");
+    li.id = todo.id;
+    const span = document.createElement("span");
+    span.innerText = todo.text;
+    const button = document.createElement("button")
+    button.innerText = "❌"
+    button.addEventListener("click", delate);
+    li.appendChild(span);
+    li.appendChild(button);
+    indexLi.appendChild(li);
 }
 
 function aSubmit(event){
     event.preventDefault();
     const newTodo = input.value;
-    addTodo(newTodo)
     input.value = "";
+    const TI = { //todo and id
+        text: newTodo,
+        id: Date.now()
+    }
+    addTodo(TI)
 }
 
 form.addEventListener("submit", aSubmit);
