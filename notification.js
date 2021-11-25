0e5e62b55b1dd4a41821e104bb572de64aadab60
eunsu28 / AY_28 <77998267+eunsu28@users.noreply.github.com>
2021-11-25 11:02:44 +0800

    Add files via upload

--- notification.js
+++ notification.js
@@ -1,3 +1,5 @@
+const time = document.querySelector("#clock")
+
 function showNotification(){
     const notification = new Notification("와우", {
         body: "holololo"
@@ -8,7 +10,9 @@ function showNotification(){
 console.log(Notification.permission);
 
 if(Notification.permission === "granted") {
-    showNotification();
+    if(time.innerText === "06:00:00") {
+        showNotification()
+    }
 } else if (Notification.permission === "denied") {
     Notification.requestPermission().then(permission => {
         console.log(permission)
