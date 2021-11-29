50ccfc5f34390b912cace55e9f4a4f27630d8d26
eunsu28 / AY_28 <77998267+eunsu28@users.noreply.github.com>
2021-11-29 18:44:40 +0800

    Add files via upload

--- notification.js
+++ notification.js
@@ -3,14 +3,13 @@ const time = document.querySelector("#clock")
 function showNotification(){
     const notification = new Notification("check your todo list", {
         body: "holololo"
-
     })
 }
 
 console.log(Notification.permission);
 
 if(Notification.permission === "granted") {
-    setInterval(showNotification, "500000")
+    setInterval(showNotification, "100000000")
 } else if (Notification.permission === "denied") {
     Notification.requestPermission().then(permission => {
         console.log(permission)
