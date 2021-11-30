f5daadf7244fadfde4ffe0e210eec32f87066f77
eunsu28 / AY_28 <77998267+eunsu28@users.noreply.github.com>
2021-11-30 09:52:28 +0800

    Add files via upload

--- notification.js
+++ notification.js
@@ -9,7 +9,7 @@ function showNotification(){
 console.log(Notification.permission);
 
 if(Notification.permission === "granted") {
-    setInterval(showNotification, "100000000")
+    setInterval(showNotification, "50000000")
 } else if (Notification.permission === "denied") {
     Notification.requestPermission().then(permission => {
         console.log(permission)
