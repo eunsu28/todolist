1d426eadbec0eec223225d256c531e9d4d2283a7
AY_28 <77998267+eunsu28@users.noreply.github.com>
2022-12-31 04:13:14 +0000

    change notification time

--- notification.js
+++ notification.js
@@ -9,7 +9,7 @@ function showNotification(){
 console.log(Notification.permission);
 
 if(Notification.permission === "granted") {
-    setInterval(showNotification, "10000000")
+    setInterval(showNotification, "900000")
 } else if (Notification.permission === "denied") {
     Notification.requestPermission().then(permission => {
         console.log(permission)
